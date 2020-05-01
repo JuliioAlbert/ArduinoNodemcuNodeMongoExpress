073bd79549c4040eeb33e85b0f3687b40fa8f749
julio <juliioalbert98@gmail.com>
2020-04-30 23:36:34 -0500

    Segundo

--- src/config/bd.js
+++ src/config/bd.js
@@ -4,11 +4,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 var urlDB;
 
-urlDB = 'mongodb://localhost:27017/NoSql-2020';
-/* if (process.env.NODE_ENV === 'dev') {
-} else {
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/NoSql-2020';
+} /* else {
     urlDB = 'mongodb+srv://juli:juli@cluster0-ugleq.mongodb.net/milibrotec';
-} */
+}  */
 process.env.URLDB = urlDB;
 //db mongo url
 //mongodb+srv://juli:juli@cluster0-ugleq.mongodb.net/milibrotec
