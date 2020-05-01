ac17188ee1c401b36322993d55f5c40e4c876b0d
julio <juliioalbert98@gmail.com>
2020-05-01 00:38:23 -0500

    Seg

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+require('cors');
 
+
+//Acceso CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+    next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -22,13 +31,7 @@ mongoose.connect(process.env.URLDB, {
     console.log('Base de datos ONLINE');
 });
 
-//Acceso CORS
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-    next();
-});
+
 
 app.listen(process.env.PORT, () => { console.log('Escuchando el puerto: ', process.env.PORT); });
 
