4e1b74ec54cefb36c2a7a629a98730e4a4bcc983
julio <juliioalbert98@gmail.com>
2020-04-30 23:59:49 -0500

    Segundo

--- src/config/bd.js
+++ src/config/bd.js
@@ -6,9 +6,11 @@ var urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/NoSql-2020';
-} /* else {
-    urlDB = 'mongodb+srv://juli:juli@cluster0-ugleq.mongodb.net/milibrotec';
-}  */
+}  else {
+    urlDB = 'mongodb+srv://julioA:julio@cluster0-99l1o.mongodb.net/NOSQL';
+}  
 process.env.URLDB = urlDB;
 //db mongo url
+//mongodb+srv://julioA:julio@cluster0-99l1o.mongodb.net/NOSQL
 //mongodb+srv://juli:juli@cluster0-ugleq.mongodb.net/milibrotec
+//mongodb+srv://julioA:julio@cluster0-99l1o.mongodb.net/NOSQL
